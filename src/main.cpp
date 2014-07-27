d691f731445fbe4799e7f58354db272eeb764267
Maluna <manu3493@hotmail.fr>
2014-07-27 14:55:39 +0200

    Création du main du projet

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*************************************
+ * @file    main.cpp
+ * @date    27/07/14
+ * @author  Manuel
+ *
+ * Fonction main du projet.
+ * Lance l'application.
+ *************************************
+*/
+
+#include <iostream>
+
+int main(void)
+{
+  std::cout << "Player" << std::endl;
+
+  return 0;
+}
