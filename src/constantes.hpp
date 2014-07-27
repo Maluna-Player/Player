8468f3234ab41322c04fed2e5a77bc372c388332
Maluna <manu3493@hotmail.fr>
2014-07-27 15:17:15 +0200

    Création de la fenêtre principale * Création de la fenêtre * Ajout de la boucle d'événements * Ajout des fonctions d'effacement et d'affichage de la fenêtre

---
+++ src/constantes.hpp
@@ -0,0 +1,23 @@
+/*************************************
+ * @file    constantes.hpp
+ * @date    27/07/14
+ * @author  Manuel
+ *
+ * Contient l'ensemble des constantes
+ * de préprocesseur du projet.
+ *************************************
+*/
+
+#ifndef __CONSTANTES_HPP__
+#define __CONSTANTES_HPP__
+
+/** Fenêtre de l'application **/
+
+// Taille de la fenêtre du player
+#define LARGEUR_FENETRE       800
+#define HAUTEUR_FENETRE       600
+
+// Titre de la fenêtre
+#define TITRE_FENETRE         "Player"
+
+#endif  // __CONSTANTES_HPP__
--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,29 @@
  *************************************
 */
 
-#include <iostream>
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include "constantes.hpp"
 
 int main(void)
 {
-  std::cout << "Player" << std::endl;
+  sf::RenderWindow window(sf::VideoMode(LARGEUR_FENETRE, HAUTEUR_FENETRE), TITRE_FENETRE);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear(sf::Color::Black);
+
+    window.display();
+  }
 
   return 0;
 }
