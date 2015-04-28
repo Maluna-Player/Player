c3f216a0f5e4d431ee7d164278a0116c224d1112
Maluna <manu3493@hotmail.fr>
2015-04-28 20:17:59 +0200

    Gestion du temps et changement de musique

--- Player/PlayerWindow.cpp
+++ Player/PlayerWindow.cpp
@@ -12,13 +12,13 @@
 #include "Path.h"
 
 PlayerWindow::PlayerWindow(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent), m_TimerId(0)
 {
     setWindowTitle(tr(WINDOW_TITLE));
     resize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    m_Player.loadSongs(SONGS_SUBDIR);
-    m_Player.play();
+    refreshSongsList();
+    setState(PLAY_STATE);
 }
 
 // ==============================
@@ -27,4 +27,91 @@ PlayerWindow::PlayerWindow(QWidget *parent)
 PlayerWindow::~PlayerWindow()
 {
     FmodManager::deleteInstance();
+}
+
+// ==============================
+// ==============================
+
+void PlayerWindow::setState(State_t state)
+{
+    if (state == PLAY_STATE)
+        m_Player.play();
+    else if (state == PAUSE_STATE)
+        m_Player.pause();
+    else if (state == STOP_STATE)
+        m_Player.stop();
+}
+
+// ==============================
+// ==============================
+
+void PlayerWindow::changeSong(int song)
+{
+    if (song != UNDEFINED_SONG)
+    {
+        try
+        {
+            m_Player.changeSong(song);
+
+            if (m_Player.isPaused())
+                setState(STOP_STATE);
+        }
+        catch(StreamError_t error)
+        {
+            refreshSongsList();
+        }
+    }
+    else
+    {
+        if (!m_Player.isStopped())
+            setState(STOP_STATE);
+    }
+}
+
+// ==============================
+// ==============================
+
+void PlayerWindow::refreshSongsList()
+{
+    m_Player.loadSongs(SONGS_SUBDIR);
+
+    changeSong(m_Player.first());
+
+    if (!m_Player.isStopped())
+      setState(STOP_STATE);
+}
+
+// ==============================
+// ==============================
+
+void PlayerWindow::timerEvent(QTimerEvent *event)
+{
+    if (event->timerId() == m_TimerId)
+    {
+        if (m_Player.isPlayed())
+        {
+            if (m_Player.getCurrentSong().isFinished())
+                changeSong(m_Player.next());
+        }
+    }
+    else
+    {
+        QWidget::timerEvent(event);
+    }
+}
+
+// ==============================
+// ==============================
+
+void PlayerWindow::showEvent(QShowEvent* /*event*/)
+{
+    m_TimerId = startTimer(REFRESH_TIME_MS);
+}
+
+// ==============================
+// ==============================
+
+void PlayerWindow::hideEvent(QHideEvent* /*event*/)
+{
+    killTimer(m_TimerId);
 }
--- Player/PlayerWindow.h
+++ Player/PlayerWindow.h
@@ -14,13 +14,45 @@
 #include <QWidget>
 #include "Player.h"
 
+#include <QTimerEvent>
+#include <QShowEvent>
+#include <QHideEvent>
+
 class PlayerWindow : public QWidget
 {
     Q_OBJECT
 
     private:
 
+        int m_TimerId;
+
         Player m_Player;
+
+
+        /**
+         * @brief Lance le son song du player et actualise l'interface graphique (titre, durée...)
+         * @param song Indice de la musique
+         */
+        virtual void changeSong(int song);
+
+        /**
+         * @brief Change l'état du player et modifie l'affichage.
+         * @param state Nouvel état du player
+         */
+        virtual void setState(State_t state);
+
+        /**
+         * @brief Met à jour la liste des musiques du répertoire.
+         */
+        virtual void refreshSongsList();
+
+    protected:
+
+        virtual void timerEvent(QTimerEvent *event);
+
+        virtual void showEvent(QShowEvent *event);
+
+        virtual void hideEvent(QHideEvent *event);
 
     public:
 
